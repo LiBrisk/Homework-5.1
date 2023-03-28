b443ded4e014c2783edc9491e75855f5e9feb36e
LiBrisk <kseniavolga3421@gmail.com>
2023-03-28 22:15:36 +0200

    Added CSV

--- src/main/java/ru/netology/homework/services/FreelancerService.java
+++ src/main/java/ru/netology/homework/services/FreelancerService.java
@@ -10,9 +10,10 @@ public class FreelancerService {
                 count++; // увеличиваем счётчик месяцев отдыха
                 money = (money - expenses) / 3;
             } else {
-                money = money + income- expenses;
+                money = money + income - expenses;
             }
         }
         return count;
     }
+
 }
--- src/test/java/FreelancerServiceTest.java
+++ src/test/java/FreelancerServiceTest.java
@@ -1,17 +1,43 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.homework.services.FreelancerService;
 
 public class FreelancerServiceTest {
-@Test
-void shouldCalculateAmount (){
-    FreelancerService service = new FreelancerService();
-    int income = 10_000;
-    int expenses = 3_000;
-    int threshold = 20_000;
-    int expected = 3;
+    @Test
+    void shouldCalculateAmount() {
+        FreelancerService service = new FreelancerService();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
 
-    int actual = service.calculate(10000, 3000, 20000);
-    Assertions.assertEquals(expected, actual);
+        int actual = service.calculate(10000, 3000, 20000);
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldCalculateAmount2() {
+        FreelancerService service = new FreelancerService();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        int actual = service.calculate(100000, 60000, 150000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/free.csv")
+    public void shouldCalculateAmount3(int income, int expenses, int threshold, int expected) {
+        FreelancerService service = new FreelancerService();
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
 }
